fe75ceaa4cf7b2c2db1c2ba3cd21e5f5dc04731d
Patrik Burda <burda.patrik@email.cz>
2017-03-19 16:43:05 +0100

    Add NUnit, Resharper, First Test

---
+++ IW5-project/TestingDemo/DBTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using IW5_project;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TestingDemo
+{
+    [TestFixture]
+    public class DBTest
+    {
+        [Test]
+        public void ShouldInsertData()
+        {
+            DBEntities db = new DBEntities();
+
+            student student = new student();
+
+            student.name = "Patrik";
+            student.surname = "Burda";
+            student.username = "Kamil";
+            student.password = "Poruba";
+            student.photo = "path";
+
+            db.students.Add(student);
+            db.SaveChanges();
+
+            Assert.Pass("student data insert passed");
+        }
+    }
+}
